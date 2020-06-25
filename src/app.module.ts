2fbfcfe5705713ddc73d17d51369ce07769364b9
Amit Kumar <amiku.cse@gmail.com>
2020-06-25 17:11:24 +0530

    nest g controller ingredient

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IngredientController } from './ingredient/ingredient.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, IngredientController],
   providers: [AppService],
 })
 export class AppModule {}
