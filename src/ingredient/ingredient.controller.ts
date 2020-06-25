eb9ac4a4947c455ab222b1c88bd452d2df2914dc
Amit Kumar <amiku.cse@gmail.com>
2020-06-25 17:21:49 +0530

    Added ingredient implementation

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Req} from '@nestjs/common';
+import {Request} from 'express';
+import {Ingredient} from './ingredient.dto';
 
 @Controller('ingredient')
-export class IngredientController {}
+export class IngredientController {
+    @Get()
+    getAllIngredients(@Req() request: Request): string {
+        console.log('request', request);
+        return "All ingredients";
+    }
+
+    @Get(':id')
+    getOneIngredient(@Param('id') id: string): string {
+        return `Return one ingredient with id: ${id}`;
+    }
+
+    @Post()
+    addIngredient(@Body() ingredientDTO: Ingredient) {
+        console.log(ingredientDTO);
+        return ingredientDTO;
+    }
+}
---
+++ src/ingredient/ingredient.dto.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    englishName: string;
+    hindiName: string;
+    possibleUnits: string[];
+}
