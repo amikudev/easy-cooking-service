ac5c78c9e81d15335de8dee7910ed3ff3b9d994c
Amit Kumar <amiku.cse@gmail.com>
2020-06-27 19:56:38 +0530

    Adding missing dependencies and getting the app working again with added mangoose dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { IngredientController } from './ingredient/ingredient.controller';
 import { RecipeController } from './recipe/recipe.controller';
 
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
   controllers: [AppController, IngredientController, RecipeController],
   providers: [AppService],
+  exports: []
 })
 export class AppModule {}
