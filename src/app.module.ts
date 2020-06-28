f0dc72f4392ebb1d6fc67e329bc930d13e0b6970
Amit Kumar <amiku.cse@gmail.com>
2020-06-28 18:17:24 +0530

    disabled mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { LoggerService } from './logger/logger.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    // MongooseModule.forRoot('mongodb://localhost/nest'),
     RecipeModule,
     IngredientModule
   ],
